e5bd28d0679223554d5ab0115cb84009834df43a
jessicayyu <jessyu2010@gmail.com>
2020-02-01 23:14:52 -0800

    Amazon, Ebay, Reddit URL shortener
    Creates a shortened URL for the current product page or main post, and
    copies it to clipboard.

---
+++ url-shortener-amazon-ebay-reddit.js
@@ -0,0 +1,23 @@
+javascript: (function() {
+    let url = new URL(window.location.href);
+    let paste;
+    let matchStr = url.pathname;
+    let host = url.hostname;
+    if (host.indexOf("amazon") > 0) {
+        matchStr = matchStr.split('/dp/')[1].match(/\w{10}/)[0];
+        paste = "amzn.com/" + matchStr;
+    } else if (host.indexOf("ebay") > 0) {
+        matchStr = matchStr.split('/');
+        matchStr = matchStr[matchStr.length - 1];
+        paste = "ebay.com/itm/" + matchStr;
+    } else if (host.indexOf("reddit.com") > 0) {
+        matchStr = matchStr.split('/comments/')[1].match(/\w{6}/)[0];
+        paste = "redd.it/" + matchStr;
+    }
+    const ele = document.createElement('textarea');
+    ele.value = paste;
+    document.body.appendChild(ele);
+    ele.select();
+    document.execCommand('copy');
+    document.body.removeChild(ele);
+}())
